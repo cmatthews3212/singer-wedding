1a991456fd75980e43fb57fca95c554e631e3bc4
Christine Matthews <candreola@gmail.com>
2024-10-18 14:11:14 -0500

    make registry button

--- src/components/pages/Registry.jsx
+++ src/components/pages/Registry.jsx
@@ -3,8 +3,8 @@ export default function Registry () {
         <div className="content">
             <div className="registry">
             <h2>Wedding Registry</h2>
-            <p>Click the link below to view our wedding registry if you are interested in blessing us as we start our new life together!</p>
-            <a className="registry-link" target="_blank" href="https://www.amazon.com/wedding/registry/10OKECB6NEQGO?ref=wr_search_page_result_3">Wedding Registry Link</a>
+            <p>Click below to view our wedding registry if you are interested in blessing us as we start our new life together!</p>
+            <a className="registry-link" target="_blank" href="https://www.amazon.com/wedding/registry/10OKECB6NEQGO?ref=wr_search_page_result_3">Wedding Registry</a>
 
             </div>
         </div>
